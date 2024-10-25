89afd6716fe961737a28bfe5839ee4f37d68434e
bzorzet98 <bzorzet@sinc.unl.edu.ar>
2024-10-25 15:32:45 -0300

    2024-10-25. Se volvio a guardar

--- global_config.py
+++ global_config.py
@@ -3,3 +3,4 @@ import os
 # PATH TO SAVE DATA ADQUISITION
 PATH_TO_SAVE_DATA_EEG_MI = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'eeg_mi')
 PATH_TO_SAVE_DATA_EEG_SSVEP = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'eeg_ssvep')
+
